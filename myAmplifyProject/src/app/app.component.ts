4a637b6cbf7f89bf402f4e3df6d6ce7546410ea4
NiverMtz <nivermartinezmed@gmail.com>
2020-09-19 18:06:32 -0500

    Primera versión, otro intento con ionic

---
+++ myAmplifyProject/src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+
+import { Platform } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+import { APIService } from './API.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss']
+})
+export class AppComponent {
+  constructor(
+    private platform: Platform,
+    private splashScreen: SplashScreen,
+    private statusBar: StatusBar,
+    private apiService: APIService
+  ) {
+    this.initializeApp();
+  }
+
+  createTodo() {
+    this.apiService.CreateTodo({
+        name: 'ionic',
+        description: 'testing'
+    });
+  }
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+    });
+  }
+}
